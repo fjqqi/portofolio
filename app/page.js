d29a94d2a3037627641e64e57bb25b798c8dd350
viqqib <faqihzaky43@gmail.com>
2025-02-28 16:29:36 +0800

    fixing image components errors 3

--- app/page.js
+++ app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
             <p className="text-6xl mt-4">A * Web Developer</p>
 
             <p className="text-xl w-[80%] mt-9">
-              I'm passionate about crafting experiences that are engaging,
-              accessible, clean and user-centric. btw i listen to music 24/7.
+              I&apos;m passionate about crafting experiences that are engaging,
+              accessible, clean and user-centric. btw I listen to music 24/7.
             </p>
           </div>
 
@@ -29,23 +29,35 @@ export default function Home() {
               <div className="h-[486px]  w-fit p-5 rounded-[64px]    relative justify-center ">
                 <div className="frame">
                   <Image
-                    src={"https://awsimages.detik.net.id/community/media/visual/2019/10/10/bb1bf3b6-6460-46dd-9de4-f7baea9c0afc_34.jpeg?w=1200"}
+                    src={
+                      "https://awsimages.detik.net.id/community/media/visual/2019/10/10/bb1bf3b6-6460-46dd-9de4-f7baea9c0afc_34.jpeg?w=1200"
+                    }
                     className=""
                     width={500}
                     height={500}
                     alt="@fjqqi"
                   />
-
                 </div>
 
                 <div className=" text-black bottom-12 left-0 right-0 min-w-full h-20 absolute px-4">
-                  
-                  <Tag text={'Fiqqi Basri'} link={'https://www.facebook.com/fjqqi/'} icon={ <FaFacebook className="size-6" />} className='bg-blueTag right-6 rotate-[5deg]'/>
-                  <Tag text={'Fjqqi'} icon={ <FaGithub className="size-6 " />} className={`bg-purpleTag right-16 rotate-[8deg] top-16`}/>
-                  <Tag text={'@fjqqi'} icon={ <FaInstagram className="size-6" />} className={`left-8 bg-orangeTag top-16 rotate-[-10deg]`}/>
+                  <Tag
+                    text={"Fiqqi Basri"}
+                    link={"https://www.facebook.com/fjqqi/"}
+                    icon={<FaFacebook className="size-6" />}
+                    className="bg-blueTag right-6 rotate-[5deg]"
+                  />
+                  <Tag
+                    text={"Fjqqi"}
+                    icon={<FaGithub className="size-6 " />}
+                    className={`bg-purpleTag right-16 rotate-[8deg] top-16`}
+                  />
+                  <Tag
+                    text={"@fjqqi"}
+                    icon={<FaInstagram className="size-6" />}
+                    className={`left-8 bg-orangeTag top-16 rotate-[-10deg]`}
+                  />
                 </div>
               </div>
-
             </div>
             {/* <p className="text-6xl mt-4">A * Web Developer</p>
 
